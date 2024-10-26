d328a6ed77963ee5ed81d6da8b12506d9c1b5f92
mukeshkumargit <127770492+mukeshkumargit@users.noreply.github.com>
2024-10-26 21:49:26 +0530

    added comment

---
+++ src/JavaSessions/SumNumber.java
@@ -0,0 +1,15 @@
+package JavaSessions;
+
+public class SumNumber {
+
+	public static void main(String[] args) {
+
+		// added comment : to sum of two numbers
+		int a1 = 10, b1 = 20, sum;
+
+		sum = a1 + b1;
+		System.out.println("Sum of Number : " + sum);
+
+	}
+
+}
